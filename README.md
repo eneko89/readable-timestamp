6efb9f79647395b5b253616c23e8982512393745
Eneko <contact@eneko.me>
2016-03-31 18:13:05 +0200

    Initial commit.

---
+++ README.md
@@ -0,0 +1,40 @@
+time-ago
+========
+
+Generates a human readable timestamp that tells how many time has elapsed since
+a given date until now. If more than 30 days have elapsed, generates a short date
+string: day + abbreviated month + year (last one only if the given date it's not
+in the current year).
+
+Elapsed time | Output example
+------------ | --------------
+Less than a minute | Just now
+One minute | A minute ago
+Between 1 - 60 minutes | 7 minutes ago
+One hour | An hour ago
+Between 1 - 24 hours | 13 hours ago
+One day | One day ago
+Between 1 - 30 days | 4 days ago
+More than a month, but in the current year | 23 Feb
+More than a month, but in another year | 9 Dec 2015
+
+
+Usage
+-----
+
+Works both required as CommonsJS module in node or in the browser.
+
+
+```javascript
+
+var ago = require('time-ago');
+
+var now = new Date();
+
+// It will log 'Just now'.
+console.log(ago(now));
+
+```
+
+As a CommonsJS module it exports a function, and in a browser environment
+declares ago() function in the global scope.
---
+++ index.js
@@ -0,0 +1,121 @@
+/*!
+ * Copyright © 2016 Eneko Sanz <contact@eneko.me>
+ * File distributed under the MIT license.
+ *
+ * Description:
+ * Generates a human readable timestamp that tells how many time
+ * has elapsed since a given date until now.
+ */
+
+(function() {
+
+  'use strict';
+
+  /**
+   * Generates a human readable timestamp that tells how many time
+   * has elapsed since 'date' until now. If more than 30 days have
+   * elapsed, generates a short date string. Some output examples:
+   * '2 minutes ago', 'An hour ago', '3 days ago', '23 Feb'...
+   * 
+   * @param  {Date}    date  Date object.
+   * 
+   * @return {String}        A string with the elapsed time.
+   */
+  function ago(date) {
+
+    // Time constants.
+    var SECS_IN_A_MINUTE = 60,
+        SECS_IN_AN_HOUR = SECS_IN_A_MINUTE * 60,
+        SECS_IN_A_DAY = SECS_IN_AN_HOUR * 24,
+        SECS_IN_A_MONTH = SECS_IN_A_DAY * 30;
+
+    // Date and time now.
+    var currentDate = new Date();
+    
+    // Time elapsed since 'date' in seconds.
+    var elapsed = (currentDate.getTime() / 1000)
+                  - (date.getTime() / 1000);
+
+    if (elapsed < SECS_IN_A_DAY) {
+
+      // Only a few seconds have elapsed; that's virtually now.
+      if (elapsed < SECS_IN_A_MINUTE) {
+        return 'Just now';
+      } else {
+
+        // Less than an hour has elapsed, so we return elapsed
+        // time in minutes.
+        if (elapsed < SECS_IN_AN_HOUR) {
+          var mins = Math.round(elapsed / SECS_IN_A_MINUTE);
+          if (mins === 1) {
+            return 'A minute ago';
+          } else {
+            return  mins + ' minutes ago';
+          }
+        } else {
+
+          // Less than a day but more than an hour has elapsed,
+          // so we return elapsed time in hours.
+          var hours = Math.round(elapsed / SECS_IN_AN_HOUR);
+          if (hours === 1) {
+            return 'An hour ago';
+          } else {
+            return  hours + ' hours ago';
+          }
+        }
+      }
+    } else {
+      if (elapsed < SECS_IN_A_MONTH) {
+
+        // Less than a month but more than a day has elapsed,
+        // so we return elapsed time in days.
+        var days = Math.round(elapsed / SECS_IN_A_DAY);
+        if (hours === 1) {
+          return 'One day ago';
+        } else {
+          return  days + ' days ago';
+        }
+      } else {
+
+        // More than 30 days have elapsed, so we erite a short
+        // date representation (day + abbreviated month).
+        var shortDate = date.getDate() + ' '
+                        + getShortMonth(date.getMonth());
+
+        // If date is not from the current year, include it in
+        // the date also (day + abbreviated month + year).
+        if (currentDate.getFullYear() !== date.getFullYear()) {
+          shortDate += ' ' + date.getFullYear();
+        }
+        return shortDate;
+      }
+    }
+
+    /**
+     * Returns the abbreviated name of the month corresponding to
+     * the month index passed as parameter (0-January, 1-February,
+     * and so forth).
+     * 
+     * @param  {Number}  month  Zero-based month index.
+     * 
+     * @return {String}         Abbreviated name of the month.
+     */
+    function getShortMonth(month) {
+      var months = ['Jan','Feb','Mar','Apr','May','Jun',
+                    'Jul','Aug','Sep','Oct','Nov', 'Dec'];
+      return months[month];
+    }
+  }
+
+  // Check if we are in a browser or node.js environment.
+  if (typeof module !== 'undefined' && module.exports) {
+
+    // Export as CommonJS module.
+    module.exports = ago;
+  } else {
+
+    // Export it as 'ago' global variable.
+    this.ago = ago;
+  }
+
+}).call(this);
